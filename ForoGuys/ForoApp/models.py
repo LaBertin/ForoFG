fe36d8a58344a3e1d4628a4a18578ddf8d1a6415
JudioGG <Smexyisrael2@gmail.com>
2020-10-18 20:27:13 -0300

    ACTUALIZACION LAZARIO 1.1

--- ForoGuys/ForoApp/models.py
+++ ForoGuys/ForoApp/models.py
@@ -3,14 +3,15 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Código_FallGuys = models.CharField(max_length=4)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField(max_length=500)
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
 def publish(self):
-    self.published_date = timezone.now()
+    self.Fecha_Publicación = timezone.now()
     self.save()
 
 def __str__(self):
