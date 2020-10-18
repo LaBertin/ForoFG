a6e29b823c942ed6fc9e039600dd19aeca653f5a
Lázaro Bertín <la.bertin.duoc.cl>
2020-10-18 19:46:53 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- ForoGuys/ForoApp/models.py
+++ ForoGuys/ForoApp/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    Autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    Código_FallGuys = models.CharField(max_length=4)
-    Título = models.CharField(max_length=200)
-    Texto = models.TextField(max_length=500)
-    Fecha_Creación = models.DateTimeField(
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
         default=timezone.now)
-    Fecha_Publicación = models.DateTimeField(
+    published_date = models.DateTimeField(
         blank=True, null=True)
 def publish(self):
-    self.Fecha_Publicación = timezone.now()
+    self.published_date = timezone.now()
     self.save()
 
 def __str__(self):
