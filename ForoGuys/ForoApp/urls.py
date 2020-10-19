d06fa6b73fce43812eaab1ba0fd8fdbcb99a822d
JudioGG <Smexyisrael2@gmail.com>
2020-10-18 23:22:12 -0300

    BASE REALIZADA.

--- ForoGuys/ForoApp/urls.py
+++ ForoGuys/ForoApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,name='detalle_publicacion'),
+    url(r'^post/new/$', views.post_new, name='nuevo_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='editar'),
 ]
