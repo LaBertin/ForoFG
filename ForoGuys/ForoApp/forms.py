7571c441d2309461e167520306bf2e51c906bae9
JudioGG <Smexyisrael2@gmail.com>
2020-10-29 16:18:22 -0300

    placeholder OK

--- ForoGuys/ForoApp/forms.py
+++ ForoGuys/ForoApp/forms.py
@@ -15,7 +15,13 @@ class UsuariosFrom(forms.ModelForm):
         fields = ('nom_usuario','codigo_fallGuy', 'email', 'contrasena')
 
 class Raw(forms.Form):
-    nombre_usuario = forms.CharField()
-    codigo_fallGuy = forms.CharField()
-    email = forms.CharField()
-    contrasena = forms.CharField()
+    nombre_usuario = forms.CharField(label='Nombre de usuario',
+                                     widget=forms.TextInput(attrs={"placeholder":"Ingrese nombre de usuario"}))
+    codigo_fallGuy = forms.CharField(label='Codigo FallGuy',
+                                     widget=forms.TextInput(attrs={"placeholder":"Codigo FallGuy #"}))
+    email = forms.CharField(label='Email',
+                                     widget=forms.TextInput(attrs={"placeholder":"Ingrese correo electronico"}))
+    contrasena = forms.CharField(label='Contraseña',
+                                     widget=forms.TextInput(attrs={"placeholder":"Ingrese contraseña"}))
+    repetirContrasena = forms.CharField(label='Confirmar contraseña',
+                                     widget=forms.TextInput(attrs={"placeholder":"Ingrese nuevamente contraseña"}))
