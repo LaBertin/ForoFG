0456daf41c4be73132b5d65e19d2db4f12a4463f
JudioGG <Smexyisrael2@gmail.com>
2020-12-06 22:00:44 -0300

    Service worker operando

--- ForoGuys/ForoGuys/settings.py
+++ ForoGuys/ForoGuys/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'serviceworker.js')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'pwa'
 ]
 
 MIDDLEWARE = [
